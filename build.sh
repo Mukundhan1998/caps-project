3bf868c180b7aa7e3d151c9e8baaeea3de0a5771
Mukundhan1998 <mukundhanr98@gmail.com>
2025-04-05 16:06:04 +0530

    tag commit

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 IMAGE_NAME="mukundhan20/second-app"
 
 # Build the Docker image
-docker build -t $IMAGE_NAME:latest .
+docker build -t mukundhan20/second-app:latest ./build
 
 # Tag the image for the dev repository
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:dev
